36f0e5ea7b43f50de6b13641064f372ed1a0854f
nikalinov <nikitakalinovskij@gmail.com>
2023-09-10 17:39:46 +0100

    Added enough results extraction (when no. passed >10)

--- api/management/commands/find.py
+++ api/management/commands/find.py
@@ -2,6 +2,7 @@ from selenium.webdriver import Firefox, FirefoxOptions
 from selenium.webdriver.common.by import By
 from django.core.management.base import BaseCommand
 from urllib.parse import urlencode
+from time import sleep
 
 
 URL = 'https://glasgow.summon.serialssolutions.com/?s.'
@@ -27,18 +28,15 @@ class Command(BaseCommand):
         for option in FILTERS:
             driver.find_element(By.LINK_TEXT, option).click()
 
-        """while True:
-            numbers_elems = driver.find_elements(By.CLASS_NAME, 'resultNumber ng-binding bg-scope')
-            numbers = [elem.text for elem in numbers_elems]
-            print(numbers_elems)
-            print()
-            if numbers[-1] >= kwargs['number']:
+        while True:
+            numbers_elems = driver.find_elements(By.CSS_SELECTOR, '.resultNumber.ng-binding.ng-scope')
+            last_number = int(numbers_elems[-1].text)
+            print(last_number)
+            if last_number >= kwargs['number']:
+                print('finish')
                 break
-            driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')"""
-
-        numbers_elems = driver.find_elements(By.CSS_SELECTOR, '.resultNumber.ng-binding.bg-scope')
-        numbers = [elem.text for elem in numbers_elems]
+            driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+            sleep(5)
 
         # results = driver.find_element(By.ID, 'results').find_element(By.CLASS_NAME, 'inner')
-
-        #driver.quit()
+        # driver.quit()
